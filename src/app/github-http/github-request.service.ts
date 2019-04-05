7dc17bfbd46cc17e3e1de91046c02120527ad635
klaus <oriokiklaus@gmail.com>
2019-04-05 12:51:58 +0300

    add newRepo instance and assgn it to the API response, also add new arguments to user class and their corresponding properties in the API response

--- src/app/github-http/github-request.service.ts
+++ src/app/github-http/github-request.service.ts
@@ -4,6 +4,7 @@ import {environment} from '../../environments/environment'
 import {User} from '../userInfo-class/user'
 import {Repo} from '../repoClass/repo'
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,10 +12,12 @@ export class GithubRequestService {
   user:User;
   repo:Repo;
   apiKey:any[];
+  newRepo:any;
   private userName:string;
 
   constructor(private http:HttpClient) {
-     this.user=new User("","","","","");
+     this.user=new User("","","","","","","","","");
+     //this.repo=new Repo("","","","");
   }
   userRequest(){
 
@@ -24,6 +27,11 @@ export class GithubRequestService {
       followers: string
       following: string
       avatar_url:string
+      repos_url:string
+      bio:string
+      hireable:string
+      company:string
+
 
     }
     let promise =new Promise((resolve,reject)=>{
@@ -33,8 +41,12 @@ export class GithubRequestService {
             this.user.repositories=response.public_repos
             this.user.followers=response.followers
             this.user.following=response.following
-
             this.user.image=response.avatar_url
+            this.user.repos_url=response.repos_url
+            this.user.bio=response.bio
+            this.user.hireable=response.hireable
+            this.user.company=response.company
+
 
             resolve()
           },
@@ -51,30 +63,30 @@ export class GithubRequestService {
 
         interface ApiResponse{
           name:string;
-          html_url:string
-          description: string
-          created_at: string
-
-
+          html_url:string;
+          description: string;
+          created_at: Date;
         }
         let promise =new Promise((resolve,reject)=>{
-            this.http.get<ApiResponse>('https://api.github.com/users/'+ this.userName+ '/repo?access_token='+ '5a34bd337bcd326cdc0c71d3df54f2a07b73563e').toPromise().then(response=>{
-
-                this.repo.name=response.name
-                this.repo.html_url=response.html_url
-                this.repo.description=response.description
-                this.repo.created_at=response.created_at
+            this.http.get<ApiResponse>('https://api.github.com/users/'+ this.userName+ '/repos?access_token='+ '5a34bd337bcd326cdc0c71d3df54f2a07b73563e').toPromise().then(response=>{
+                this.newRepo=response
+              //  this.repo.name=response.name
+              //  this.repo.html_url=response.html_url
+              //  this.repo.description=response.description
+              //  this.repo.created_at=response.created_at
 
                 resolve()
               },
               error=>{
-                  console.log("Not found")
+                this.newRepo="Repo not found"
+                //  this.repo.name= " Repo Not found"
                     reject(error)
                 }
             )
         })
             return promise
   }
+
   updateUsername(userName:string){
     this.userName=userName
   }
